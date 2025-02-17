be5566cc1e8795fa18342c283aa9d209010821e7
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-02-18 02:02:43 +0530

    Update: Navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg_color: "#e7eeee"
+      }
+    },
   },
   plugins: [],
 }
