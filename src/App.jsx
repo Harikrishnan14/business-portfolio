bbef5fab51f9f9f6b2f777b9aa1c67c6f6e9f996
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-02-19 23:03:53 +0530

    Update: section two

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from 'react'
 import Nav from './components/Nav'
 import SectionOne from './components/SectionOne'
+import SectionTwo from './components/SectionTwo'
 
 function App() {
   return (
     <>
       <Nav />
       <SectionOne />
+      <SectionTwo />
     </>
   )
 }
---
+++ src/components/SectionTwo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const SectionTwo = () => {
+    return (
+        <section className='py-20 px-40'>
+            <div className='py-20 flex flex-col gap-5 border-b border-gray-300'>
+                <h1 className='text-gray-800 text-6xl leading-normal font-medium'>The property that <br /> suits you perfectly</h1>
+                <p className='text-gray-700 text-3xl leading-normal'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. In laborum praesentium optio nam distinctio deserunt dolore similique dolores quo voluptate accusantium, ducimus voluptatibus delectus eaque, sint nulla exercitationem aliquid molestiae.</p>
+            </div>
+        </section>
+    )
+}
+
+export default SectionTwo
