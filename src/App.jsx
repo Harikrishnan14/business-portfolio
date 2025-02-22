e8ce0590d886919f58e590a612710b9f71b1dc0d
Harikrishnan14 <harikrishnankhd09@gmail.com>
2025-02-22 22:09:33 +0530

    Update: footer

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import SectionFour from './components/SectionFour'
 import SectionFive from './components/SectionFive'
 import SectionSix from './components/SectionSix'
 import SectionSeven from './components/SectionSeven'
+import { FaTwitter } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
 
 function App() {
   return (
@@ -19,6 +22,31 @@ function App() {
       <SectionFive />
       <SectionSix />
       <SectionSeven />
+
+      <footer className='p-20 flex items-center justify-around'>
+        <div className='font-bold text-2xl'>Binhoff.co</div>
+        <ul className='menu'>
+          <li>Product</li>
+          <li>Install</li>
+          <li>Quick start</li>
+        </ul>
+        <ul className='menu'>
+          <li>Blog</li>
+          <li>Careers</li>
+          <li>About</li>
+        </ul>
+        <div className='flex items-center gap-4'>
+          <div className="icon">
+            <FaTwitter />
+          </div>
+          <div className="icon">
+            <FaFacebookF />
+          </div>
+          <div className="icon">
+            <AiFillInstagram />
+          </div>
+        </div>
+      </footer>
     </>
   )
 }
